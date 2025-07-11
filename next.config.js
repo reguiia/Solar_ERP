01918a8263c2305f3c09478440addf3ac0b3bddb
reguiia <reguiiala@gmail.com>
2025-07-11 09:45:38 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,28 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+        path: false,
+        'utf-8-validate': false,
+        bufferutil: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
